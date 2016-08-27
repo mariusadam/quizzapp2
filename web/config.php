c2d7cc31b2c2e23cd37eeb800d7009f8ed25c767
Marius Adam <adamhoreamarius@gmail.com>
2016-08-27 10:16:42 -0500

    remote access

--- web/config.php
+++ web/config.php
@@ -14,10 +14,12 @@ if (!isset($_SERVER['HTTP_HOST'])) {
     exit('This script cannot be run from the CLI. Run it from a browser.');
 }
 
+$ok = isset($_GET['pass']) ? $_GET['pass'] === 'show-me-the-money' : false;
+
 if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
     '127.0.0.1',
     '::1',
-))) {
+))&& !$ok) {
     header('HTTP/1.0 403 Forbidden');
     exit('This script is only accessible from localhost.');
 }
